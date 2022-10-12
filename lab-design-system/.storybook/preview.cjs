a3a04b08f1501592238c708b25fdb5aec28d1b00
Tiago Martins <tiago_martins238@hotmail.com>
2022-10-12 08:21:17 -0300

    Create component Text

--- lab-design-system/.storybook/preview.cjs
+++ lab-design-system/.storybook/preview.cjs
@@ -1,5 +1,5 @@
-import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 import { themes } from '@storybook/theming'
+import '../src/styles/global.css'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- lab-design-system/tailwind.config.cjs
+++ lab-design-system/tailwind.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
       'gray-800': '#202024',
       'gray-400': '#7c7c8a',
       'gray-200': '#c4c4cc',
-      'gray-900': '#e1e1e6',
+      'gray-100': '#e1e1e6',
 
       'cyan-500': '#81d8f7',
       'cyan-300': '#9be1fb',

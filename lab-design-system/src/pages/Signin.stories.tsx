904f8f8948ea296e92be71e7cb98a8c542d4fd8a
Tiago Martins <tiago_martins238@hotmail.com>
2022-10-13 23:22:08 -0300

    Final commit

---
+++ lab-design-system/src/pages/Signin.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Signin } from "./Signin";
+
+export default {
+    title: 'pages/Sign In',
+    component: Signin,
+    args: {       
+    },
+    argTypes: {}
+} as Meta
+
+export const Default: StoryObj= {}
+
